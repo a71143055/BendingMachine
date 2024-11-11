7f33e8f8ed0f868e16ea9be644267938cc1b66dc
a71143055 <a71143055@gmail.com>
2024-11-11 12:16:16 +0900

    BendingMachine

--- BendingMachine.py
+++ BendingMachine.py
@@ -3,8 +3,8 @@ class BendingMachine:
     def __init__(self):
         self.__money = 0
         self.__menu = {
-            BB("콜라", 1000, 10),
-            BB("사이다", 1000, 10)
+            1 : BB("콜라", 1000, 10),
+            2 : BB("사이다", 1000, 10)
         }
 
     def InputMoney(self, money:int):
@@ -14,7 +14,19 @@ class BendingMachine:
     def choiceMenu(self):
         selectedMenu = int(input("메뉴를 선택하세요 : "))
         result = selectedMenu in self.__menu.keys()
+        if result == True:
+            if self.__money < self.__menu[selectedMenu].getPrice():
+                result = False
+                print("금액이 부족합니다.")
+
         return result, selectedMenu
+
+    def OutProduct(self, menu):
+        self.__menu[menu].sale()
+        print(self.__menu[menu].getName(), "가 나왔습니다.")
+
+        self.__money -= self.__menu[menu].getPrice()
+        isContinue = False
 
     def printMenu(self):
         for key, value in self.Menu.items():
--- Test.py
+++ Test.py
@@ -3,6 +3,8 @@ from BendingMachine import BendingMachine
 if __name__ == "__main__":
     BM = BendingMachine()
     BM.printMenu()
-    BM.InputMoney()
-
-    isOk, menu = BM.choiceMenu()
+    BM.InputMoney(1000)
+    isOk = False
+    while(isOk == False):
+        isOk, menu = BM.choiceMenu()
+    print("{0}번 : {1}".format(isOk,BM.choiceMenu()))
