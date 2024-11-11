ec7809a6c96d7f56ff87189e21b8bfd72d97e5f6
a71143055 <a71143055@gmail.com>
2024-11-11 11:20:35 +0900

    BendingMachine

--- bendingMachine.py
+++ bendingMachine.py
@@ -5,7 +5,4 @@ class BendingMachine:
         self.__menu = []
 
     def setMenu(self, name, price, count):
-        BB.__name = name
-        BB.__price = price
-        BB.__count = count
-        BB.__salesCount = 0
+        self.__menu += BB(name, price,count)
